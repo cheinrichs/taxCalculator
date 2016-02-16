3a2f9bee2af71fdb931f153c7421ed726b65a564
Cooper Heinrichs <cooper.heinrichs@gmail.com>
2016-02-15 17:06:58 -0700

    Added functionality to pass tests for the - range with 7% tax

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
 module.exports = {
   calcTax: function(number){
     var toBeTaxed = number;
-    var result = 0;
+    var resultTaxes = 0;
+
 
     if(toBeTaxed <= 10){
-      result = toBeTaxed * 0.10;
-    }
+      resultTaxes += (toBeTaxed * 0.1);
 
-    return result;
+    } else if( toBeTaxed > 10 && toBeTaxed < 20 ){
+       resultTaxes += 1;
+       resultTaxes += (toBeTaxed-10) * 0.07;
+       toBeTaxed = toBeTaxed % 10;
+       //console.log("left to tax " + toBeTaxed + " current taxes " + resultTaxes);
+    }
+    return resultTaxes;
   }
 }
